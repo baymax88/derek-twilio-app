16171248c3abc3b07b6eaef94e1e6e562c21eaf6
Cute-baymax <smartbay1@outlook.com>
2020-12-20 13:01:17 -0500

    config email

--- index.js
+++ index.js
@@ -104,7 +104,7 @@ app.post('/api/endMeeting', (req, res) => {
       html: `
       <link rel="preconnect" href="https://fonts.gstatic.com">
       <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
-      <h2 style="font-family: 'Roboto', sans-serif;">Please find linked the recording of our call.\n<a href="${process.env.REACT_APP_BASE_URL}/api/getMeeting?compositionsid=${composition.sid}">Get recording of meeting</a></h2>
+      <h2 style="font-family: 'Roboto', sans-serif;">Please find linked the recording of our call.\n<a href="${process.env.REACT_APP_BASE_URL}/api/getMeeting?compositionsid=${composition.sid}">Get recording of our video call</a></h2>
       `
     };
   
